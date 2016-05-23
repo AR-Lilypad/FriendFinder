c2a404d986b6853dacec4c13b95f14ea993ee8bd
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-05-22 20:46:28 -0400

    added logic

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,60 +1,58 @@
-// ===============================================================================
-// LOAD DATA: data sources hold arrays of info on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friendData 		= require('../data/friends.js');
 var path 			= require('path');
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 // API GET Requests - when users "visit" a page. 
-// In each of the below cases when a user visits a link 
-// (ex: localhost:PORT/api/admin...they are shown a JSON of the data in the table) 
+// (ex:localhost:PORT/api/admin...they are shown a JSON of the data in the table) 
 
 var totalDifference = 0;
+
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
-		res.json(friendData);
+		res.json(friends);
 	});
 
-	app.post('/api/friends', function(req, res){
-		res.json(friendsData);
-	});
-// ===============================================================================
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
+//API POST Request-handles when user submits a form & thus submits data to the server.
 // In each of the below cases, when a user submits form data (a JSON object)
 // ...the JSON is pushed to the appropriate Javascript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
 
-	app.post('/api/tables', function(req, res){
 
-// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-// It will do this by sending out the value "true" have a table 
-		if(friendData.length < 5 ){
-			friendData.push(req.body);
-			res.json(true); // KEY LINE
+	app.post('/api/friends', function(req, res){
+
+		var greatMatch = {
+			name: "",
+			image: "",
+			matchDifference: 1000
+		};
+		var usrData 	= req.body;
+		var usrName 	= usrData.name;
+		var usrImage 	= usrData.image;
+		var userTotals 	= usrData.totals;
+
+		var totalDifference = 0;
+
+		//loop through the friends data array of objects to get each friends scores
+		for(var i = 0; i < friendsData.length-1; i++){
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			//loop through that friends score and the users score and calculate the 
+			// absolute difference between the two and push that to the total difference variable set above
+			for(var j = 0; j < 10; j++){
+				// We calculate the difference between the scores and sum them into the totalDifference
+				totalDifference += Math.abs(parseInt(userTotals[j]) - parseInt(friends[i].scores[j]));
+				// If the sum of differences is less then the differences of the current "best match"
+				if (totalDifference <= bestMatch.friendDifference){
+
+					// Reset the bestMatch to be the new friend. 
+					greatMatch.name = friends[i].name;
+					greatMatch.photo = friends[i].photo;
+					greatMatch.matchDifference = totalDifference;
+				}
+			}
 		}
 
-		// Or false if they don't have a table
-		else{
-			friendData.push(req.body);
-			res.json(false); // KEY LINE
-		}
-
+		friends.push(userData);
+ 
+		res.json(greatMatch);
 	});
-
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don't worry about it!
-
-	app.post('/api/clear', function(req, res){
-		// Empty out the arrays of data
-		friendData = [];
-		friendData = [];
-
-		console.log(friendData);
-	})
-}
+};
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 // ==============================================================================
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
 
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server 
-// ==============================================================================
 
 var app = express(); // Tells node that we are creating an "express" server
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
@@ -25,7 +22,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+// These routes give our server a "map" of how to respond when users visit or 
+// request data from various URLs. 
 // ================================================================================
 
 require('./app/routing/api-routes.js')(app); 
